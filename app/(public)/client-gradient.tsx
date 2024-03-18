aac5ce3a5121e90e3500c6d8e1c4a787248509ad
gdr <gdr@duarterengifo.com>
2024-03-17 21:53:14 -0400

    fix import

--- app/(public)/client-gradient.tsx
+++ app/(public)/client-gradient.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Gradient } from './Gradient.js'
+import { Gradient } from './gradient.js'
 
 export function ClientGradient() {
 
